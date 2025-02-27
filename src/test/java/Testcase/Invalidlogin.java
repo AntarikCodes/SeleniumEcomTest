88befde5d3ae2d6f2d78df802155b9d4afcc1f7b
AntarikCodes <sinhaantarik@gmail.com>
2025-02-27 13:19:59 +0530

    init commit

--- src/test/java/Testcase/Invalidlogin.java
+++ src/test/java/Testcase/Invalidlogin.java
@@ -2,16 +2,19 @@ package Testcase;
 
 import base.BaseClass;
 import com.aventstack.extentreports.Status;
+import org.testng.annotations.Test;
 import pageEvents.loginPageEvents;
 
 import java.io.IOException;
 
-public class InvalidLogin extends BaseClass {
+public class Invalidlogin extends BaseClass {
     loginPageEvents loginPage = new loginPageEvents();
 
-    public void invalidLogin() throws IOException {
+    @Test
+    public void invalidLoginTest() throws IOException {
         logger.log(Status.INFO, "Verifying login page...");
         loginPage.verifyLoginPage();
+
         logger.log(Status.INFO,"Logging in...");
         loginPage.invalidLogin();
     }
